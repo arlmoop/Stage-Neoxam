b91d5ec63a404a9619fd6f1e723a43349ddb1260
aurelien.kammerer <>
2026-01-12 15:06:30 +0100

    feat: add exercice block3

---
+++ exercice-js/ex-block3.js
@@ -0,0 +1,14 @@
+console.log(sum(10, 20));
+
+function sum(a, b) { return a+b; }
+
+function isAdult (age) {
+    if (age >= 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(variable);
+var variable = 10;
